90ed4ce02a0bcb76dacb1db4085308a12d9b2cae
Drew Miller <drew666@gmail.com>
2015-03-13 15:54:01 -0700

    Test skeletons

---
+++ test/cold/methods.test.js
@@ -0,0 +1,13 @@
+/**
+ * create, update, subscribe, delete, et al
+ *
+ * Curious:
+ *   Can we do atomic transactions by putting create, update, delete into the same
+ *   XML?
+ *
+ * Can we handle more than 50 XML entries in the same SOAP request?
+ *
+ * Can we mix creates, updates and deletes in the same SOAP request?
+ *
+ * Is the XML case sensitive is zns:Create === zns:create ?
+ */
---
+++ test/cold/queue.test.js
@@ -0,0 +1,14 @@
+/**
+ * work queue
+ *
+ * Rate limiter.
+ *
+ * Limit the parallelism to 4 concurrent calls?
+ *
+ * Benchmark to find nice numbers?
+ *
+ * Hide the vasync work queues inside the create, update, delete
+ * calls so that the library using this doesn't have to
+ * do their own throttling so intensely.
+ *
+ */
