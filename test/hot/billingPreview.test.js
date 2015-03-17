e77de11c78dd1004870395a7ec2da097bfa2b2e1
Drew Miller <drew666@gmail.com>
2015-03-16 17:21:52 -0700

    Wrap remote exceptions nicer

--- test/hot/billingPreview.test.js
+++ test/hot/billingPreview.test.js
@@ -3,7 +3,7 @@ var zuora = require('../../zuora');
 var config = require('../../etc/config.test.json')
 
 test('billingPreview: Returns an error message', function (t) {
-  t.plan(1);
+  t.plan(4);
 
   // limits: 20 BillingPreviewRequest objects per call
   //
@@ -12,6 +12,7 @@ test('billingPreview: Returns an error message', function (t) {
   // Expected faults or errors:
   //  - InvalidTypeFault
   //  - UnexpectedErrorFault
+  //  - Unknown
   //
   //  Result may only contain Successes ..
   //    failures may request an extra call to get details
@@ -20,17 +21,21 @@ test('billingPreview: Returns an error message', function (t) {
     AccountId: 1234,
     ChargeTypeToExclude: 'OneTime,Recurring',
     TargetDate: new Date(),
-    IncludingEvergreenSubscription: false
+    IncludingEvergreenSubscription: 'false'
   }
 
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
     // ZUORA FEEDBACK: zObject is 'request' not 'BillingPreviewRequest'
-    //console.log ( Object.keys(z.request) )
+    // console.log ( Object.keys(z.request) )
     z.request.billingPreview(request, function(err, result) {
-      if (err) return t.ifError(err.message);
-      //console.log(result);
-      t.equal(result[0].Success, false, 'Success should be false. ID is missing')
+      /// Unhandled exception from Zuora:
+      t.equal(err.name, 'Remote Exception', 'Exception expected');
+      t.equal(err.message, 'Remote exception - see error.detail or client.lastMessage for remote stacktrace', 'Useless unknown message');
+      t.equal(err.code, 'Server', 'ambiguous code is Server')
+      t.ok(err.detail, 'error.detail should contain the useless java stacktrace')
+      /// Expected failure:
+      // t.equal(result[0].Success, false, 'Success should be false. ID is missing')
     })
   })
 
