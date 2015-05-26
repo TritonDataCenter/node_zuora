45bd52c9c9e9ed366245aabb2f85a74fc6d52b77
Drew Miller <drew666@gmail.com>
2015-05-26 16:08:19 -0700

    Fix escaping of ZOQL quotes
    Passes lint on test/hot/query.test.js
    
    Examples:
    'select * from account where name = "Joe\\\'s Place"'
    "select * from account where name = 'Joe\\'s Place'"

--- test/hot/query.test.js
+++ test/hot/query.test.js
@@ -1,10 +1,15 @@
+'use strict';
 var test   = require('tape');
-var zuora  = require(__dirname + '/../../zuora');
-var config = require(__dirname + '/../../etc/config.test.json');
-var log    = require(__dirname + '/../../lib/logger');
+var zuora  = require('../../zuora');
+var config = require('../../etc/config.test.json');
+var log    = require('../../lib/logger');
 
-test.skip ('query: with ZOQL syntax error fault', function (t) {
-  t.plan(2);
+test.skip('query: uses queryMore to do multiple pages of results');
+test.skip('query: adjust batchsize to 50 using QueryOptions.batchSize header');
+test.skip('query: returns a stream of results');
+
+test('query: with ZOQL syntax error fault', function (t) {
+  t.plan(4);
 
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
@@ -12,25 +17,29 @@ test.skip ('query: with ZOQL syntax error fault', function (t) {
     var zquery = 'select * from accountsX';
     z.query(zquery, function(err, result) {
       t.equal(err.message, 'You have an error in your ZOQL syntax');
+      t.equal(err.code, 'MALFORMED_QUERY', 'Expect err.code == malformed query');
+      t.notOk(result, 'Result should be empty');
+      t.end();
     });
   });
 
 });
 
-test.skip ('query: times out gracefully with adjustable timer per query', function (t) {
-  t.plan(3);
+test.skip('query: times out gracefully with adjustable timer per query', function (t) {
+  t.plan(4);
 
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
     var zquery = 'select id from invoiceitem';
     var msClockStartAt = Date.now();
     var msWait = 200;
     z.query(zquery, {requestOptions: {timeout: msWait}}, function(err, result) {
-      t.equals(err.message, 'ETIMEDOUT', 'expects ETIMEDOUT error')
+      t.equals(err.message, 'ETIMEDOUT', 'expects ETIMEDOUT error');
+      t.notOk(result, 'Result should be empty');
       var msClockEndAt = Date.now();
       var duration = msClockEndAt - msClockStartAt;
-      t.ok(duration < msWait + 30, 'can time out sooner than default')
+      t.ok(duration < msWait + 30, 'can time out sooner than default');
     });
   });
 
@@ -39,73 +48,68 @@ test.skip ('query: times out gracefully with adjustable timer per query', functi
 /**
  * Busted. The login() request stops before we start the query() request
  */
-test.skip ('query: times out gracefully with adjustable timer using client default', function (t) {
-  t.plan(3);
+test.skip('query: times out gracefully with adjustable timer using client default', function (t) {
+  t.plan(4);
 
   var msWait = 5000; // large enough to succeed
   config.requestTimeout = msWait;
   zuora.connect(config, function(err, z) {
     if (err) {
       if (err.message === 'ETIMEDOUT') {
-        t.fail('Login() should not timeout unless timer is set too short')
+        t.fail('Login() should not timeout unless timer is set too short');
       }
       return t.ifError(err.message);
     }
 
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
     msWait = 70; // small enough to fail
-    z._requestOptions
     var zquery = 'select id from invoiceitem';
     var msClockStartAt = Date.now();
     z.query(zquery, {}, function(err, result) {
-      t.equals(err.message, 'ETIMEDOUT', 'expects ETIMEDOUT error')
+      t.equals(err.message, 'ETIMEDOUT', 'expects ETIMEDOUT error');
+      t.notOk(result, 'result should be empty');
       var msClockEndAt = Date.now();
       var duration = msClockEndAt - msClockStartAt;
       t.ok(duration < msWait + 30, 'can time out sooner than default (took: ' + duration + ' ms)');
+      t.end();
     });
   });
 
 });
 
-test.skip ('query: small size request for billRuns', function (t) {
-  t.plan(2);
+test('query: small size query of billRuns', function (t) {
+  t.plan(3);
 
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
     var zquery = 'select id from billrun';
     z.query(zquery, function(err, result) {
       t.ifError(err);
-      //if (err) return t.ifError(err.message);
-      //console.dir(result);
-//      t.equal(result[0].Success, false, 'Success should be false. ID is missing');
+      t.ok(result, 'should have a result');
+      // t.equal(result[0].Success, false, 'Success should be false. ID is missing');
+      t.end();
     });
   });
 
 });
 
-test.skip ('query: medium size request for account', function (t) {
-  t.plan(2);
+test('query: medium size request for account', function (t) {
+  t.plan(3);
 
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
     var zquery = 'select id from account';
     z.query(zquery, function(err, result) {
       t.ifError(err);
-      //if (err) return t.ifError(err.message);
-      //console.dir(result);
+      t.ok(result, 'should have a result');
 //      t.equal(result[0].Success, false, 'Success should be false. ID is missing');
+      t.end();
     });
   });
 
 });
-
-test.skip('query: uses queryMore to do multiple pages of results');
-test.skip('query: adjust batchsize to 50 using QueryOptions.batchSize header');
-test.skip('query: returns a stream of results')
-test.skip('query: timesout')
-test.skip('query: simple zoql')
 
 /**
  * Steps to run this test:
@@ -115,13 +119,13 @@ test.skip('query: simple zoql')
  *
  * 3. set zuora session timeout back to something huge.
  */
-test.skip ('query: survives session timesouts', function(t) {
+test.skip('query: survives session timesouts', function(t) {
   t.plan(2);
   var q = 0;
   var maxAttempts = 17;
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
 
     function doQuery () {
       q++;
@@ -129,57 +133,63 @@ test.skip ('query: survives session timesouts', function(t) {
       z.query(zquery, function(err, result) {
         if (err) return t.ifError(err);
         if (q <= maxAttempts) {
-          setTimeout(doQuery, 60 * 1000)
+          setTimeout(doQuery, 60 * 1000);
         } else {
-          t.ok(q > maxAttempts, 'must complete all attempts')
+          t.ok(q > maxAttempts, 'must complete all attempts');
           t.equal(result[0].Success, false, 'Success should be false. ID is missing');
+          t.end();
         }
       });
     }
     doQuery();
   });
-})
+});
 
-test ('query: can use double-quotes', function(t) {
-  t.plan(3)
+test('query: can use double-quotes', function(t) {
+  t.plan(2);
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
 
     var zquery = 'select accountnumber from account where status = "Active"';
     z.query(zquery, function(err, result) {
-      log.debug( z.client._client.lastRequest );
+      log.debug(z.client._client.lastRequest);
       if (err) return t.ifError(err);
       t.ok(Array.isArray(result), 'expect valid empty results');
+      t.end();
     });
   });
-})
+});
 
-test ('query: can use double-quotes with inner apostrophy', function(t) {
-  t.plan(3)
+test('query: can use double-quotes with inner apostrophy', function(t) {
+  t.plan(2);
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
 
-    var zquery = 'select accountnumber from account where name = "bob\'s burgers"';
+    var zquery = 'select accountnumber from account where name = "bob\\\'s burgers"';
     z.query(zquery, function(err, result) {
       log.debug( z.client._client.lastRequest );
       if (err) return t.ifError(err);
       t.ok(Array.isArray(result), 'expect valid empty results');
+      t.end();
     });
   });
-})
-test ('query: can use single-quotes with inner apostrophy', function(t) {
-  t.plan(3)
+});
+
+test('query: can use single-quotes with inner apostrophy', function(t) {
+  t.plan(2);
   zuora.connect(config, function(err, z) {
     if (err) return t.ifError(err.message);
-    t.ok(z, 'connect() should return a zuora client')
+    t.ok(z, 'connect() should return a zuora client');
 
-    var zquery = "select accountnumber from account where name = 'bob\'s burgers'";
+    // DESIRED QUERY XML: ..where name = &apos;bob\\&apos;s burgers&apos;
+    var zquery = "select accountnumber from account where name = 'bob\\'s burgers'"; //eslint-disable-line quotes
     z.query(zquery, function(err, result) {
       log.debug( z.client._client.lastRequest );
       if (err) return t.ifError(err);
       t.ok(Array.isArray(result), 'expect valid empty results');
+      t.end();
     });
   });
-})
+});
